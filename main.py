c91a62244e629bc75fcce869d0a7437575d48443
Sajid Hasan Apon <sajidhasanapon@gmail.com>
2018-09-22 17:42:43 +0600

    HUGE IMPROVEMENT in runtime(1000X) using vectorization magic! UPDATE: change method name according to newer numpy version

--- main.py
+++ main.py
@@ -2,18 +2,22 @@ import pandas as pd
 from methods import *
 import pickle
 
-X_train = pd.read_excel("ratings_train.xlsx", header=None).as_matrix()  
-# X_validate = pd.read_excel("ratings_validate.xlsx", header=None).as_matrix()
-# X_test = pd.read_excel("ratings_test.xlsx", header=None).as_matrix()
+np.random.seed(100)
 
-# no separate test-dataset was provided.
-# Hence, the validation-dataset was split into validation-dataset and test-dataset
+print("Reading data...")
 
-X_validate_and_test = pd.read_excel("ratings_validate.xlsx", header=None).as_matrix()
+X_train = pd.read_excel("ratings_train.xlsx", header=None).values  
+# X_validate = pd.read_excel("ratings_validate.xlsx", header=None).values
+# X_test = pd.read_excel("ratings_test.xlsx", header=None).values
+
+"""
+ no separate test-dataset was provided.
+ Hence, the validation-dataset was split into validation-dataset and test-dataset
+"""
+
+X_validate_and_test = pd.read_excel("ratings_validate.xlsx", header=None).values
 X_validate = X_validate_and_test[0:1999] # better : [0 : int(X_validate_and_test.shape[0]/2)]
 X_test = X_validate_and_test[2000:] # better : [int(X_validate_and_test.shape[0]/2)+1 : ]
-
-
 
 best_V = None
 best_lambd = None
@@ -25,8 +29,7 @@ print("Scores: \n")
 
 for k in [10, 20, 40]:
     for lambd in [0.01, 0.1, 1, 10.0]:
-        V = train(ratings_train=X_train, n_factors=k, lambd=lambd, n_iter=1)
-        U = predict_user_factors(X_train, V, lambd)
+        U, V = train(ratings_train=X_train, n_latent_factors=k, lambd=lambd, n_iter=5)
         cost_train = get_cost(X_train, U, V)
 
         U = predict_user_factors(X_validate, V, lambd)
@@ -54,4 +57,4 @@ print(best_lambd, file=open("best_lambda.txt", "w"))
 U = predict_user_factors(X_test, best_V, best_lambd)
 cost = get_cost(X_test, U, best_V)
 print("\n\n##################################################################################\n\n")
-print("Cost = ", cost)
+print("Cost on test data = ", cost)
--- methods.py
+++ methods.py
@@ -3,14 +3,12 @@ from math import sqrt
 
 
 
-def train(ratings_train: np.ndarray, n_factors: int, lambd: float, n_iter: int=10):
-
-    np.random.seed(100)
+def train(ratings_train: np.ndarray, n_latent_factors: int, lambd: float, n_iter: int=10):
 
     X = ratings_train.copy()
     X[X == -1] = 0 # masking -1's with zeroes
 
-    V = np.random.rand(X.shape[1], n_factors)
+    V = np.random.rand(X.shape[1], n_latent_factors)
 
     for i in range(n_iter):
         U = predict_user_factors(ratings=X, item_factors=V, lambd=lambd)
@@ -18,10 +16,10 @@ def train(ratings_train: np.ndarray, n_factors: int, lambd: float, n_iter: int=1
         # cost = get_cost(ratings=ratings_train, user_factors=U, item_factors=V)
         # print(cost)
 
-    return V
+    return U, V
 
 
-def predict_user_factors(ratings: np.ndarray, item_factors: np.ndarray, lambd):
+def predict_user_factors(ratings: np.ndarray, item_factors: np.ndarray, lambd: float):
 
     if len(ratings.shape) != 2:
         print("X does not have correct shape.")
@@ -29,18 +27,28 @@ def predict_user_factors(ratings: np.ndarray, item_factors: np.ndarray, lambd):
         print("If your X has a shape of (n,), try X = np.array([X]) and then pass X.")
         exit(1)
 
-    X = ratings.copy()
-    X[X == -1] = 0
-    V = item_factors.copy()
-    m, k = X.shape[0], V.shape[1]
-    U = np.zeros((m, k))
-    right = X.dot(V)
+    X = ratings
+    V = item_factors
+    k = V.shape[1]
 
-    for i in range(m):
-        b = V[X[i] != 0]
-        left = b.T.dot(b) + lambd * np.eye(k)
-        U[i] = np.linalg.solve(left, right[i])
+    right = np.matmul(X, V) # named "right" according to instruction document
+
+    left = np.matmul(V.T, V) + lambd * np.eye(k)
+    left_inv = np.linalg.inv(left)
+
+    U = right.dot(left_inv) # note the reverse order of multiplication: right x left
     return U
+
+    """
+    I can get the whole U matrix just by transposing and altering the order of matrix multiplication.
+    No need to iterate over rows.
+
+    It took me hours to realize this simple vectorization trick.
+    Perhaps because BUET has murdered my love for Mathematics that I have become so naive.
+
+    I don't know if I'll ever get back my intuition and common sense.
+    I hope I will... someday.
+    """
 
 
 def predict_item_factors(ratings: np.ndarray, user_factors: np.ndarray, lambd: float):
@@ -49,16 +57,15 @@ def predict_item_factors(ratings: np.ndarray, user_factors: np.ndarray, lambd: f
 
 def get_cost(ratings: np.ndarray, user_factors: np.ndarray, item_factors: np.ndarray):
 
-    original_rating = ratings.copy()
+    original_ratings = ratings
     prediction      = np.matmul(user_factors, item_factors.T)
-
-    prediction[ratings == -1]         = 0
-    original_rating[ratings == -1]    = 0
 
     prediction[prediction < 0.] = 0.
     prediction[prediction > 5.] = 5.
 
-    diff            = original_rating - prediction
+    prediction[ratings == -1]         = -1 # so that difference = 0 
+
+    diff            = original_ratings - prediction
     diff_squared    = diff * diff
     total_observed  = len(ratings[ratings != -1])
     cost            = sqrt(np.sum(diff_squared) / total_observed)
